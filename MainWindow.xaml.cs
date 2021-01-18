0b2a0214b3f67d607bddd99818c8828580544b59
gui <gui>
2021-01-19 01:58:51 +0800

    selectedChanged, time encoder, decoder, apply_clicked,

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -48,6 +48,12 @@ namespace baha_skip
                 raw = reader.ReadToEnd();
             }
             timetable = JsonConvert.DeserializeObject<List<TimeTableArray>>(raw);
+            refreshListView();
+            
+        }
+        void refreshListView()
+        {
+            json_lv.ItemsSource = timetable;
         }
         public class TimeTableArray
         {
@@ -57,6 +63,128 @@ namespace baha_skip
             public string ED_Start { get; set; }
             public string ED_End { get; set; }
         }
-     
+
+        private void json_lv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TimeTableArray snInfo = (TimeTableArray)json_lv.SelectedItem;
+
+            opStart_tb.Text = TimeDecoder( snInfo.OP_Start);
+            opEnd_tb.Text = TimeDecoder(snInfo.OP_End);
+
+            edStart_tb.Text = TimeDecoder(snInfo.ED_Start);
+            edEnd_tb.Text = TimeDecoder(snInfo.ED_End);
+
+            sn_tb.Text = snInfo.sn;
+
+          TimeEncoder(snInfo.OP_End);
+        }
+        string TimeEncoder(string time)
+        {
+            string[] temp = time.Split(' ');
+            int h = 0, m = 0, s = 0;
+
+            switch (temp.Length)
+            {
+                default:
+                    throw new Exception("日期格式有誤");
+                    break;
+                case 1:
+                    s = Convert.ToInt32(temp[1]);
+                    return $"{(s).ToString()}";
+                case 2:
+                    
+                    m = Convert.ToInt32(temp[0]);
+                    s = Convert.ToInt32(temp[1]);
+                    return $"{(m).ToString()} {(s).ToString()}";
+                    
+                case 3:
+                    
+                    h = Convert.ToInt32(temp[0]);
+                    m = Convert.ToInt32(temp[1]);
+                    s = Convert.ToInt32(temp[2]);
+                    return $"{(h * 60 + m).ToString()} {(s).ToString()}"; 
+                
+
+            }
+
+        }
+        string TimeDecoder(string time)
+        {
+            string[] temp = time.Split(' ');
+            int h = 0, m = 0, s = 0;
+            switch (temp.Length)
+            {
+                default:
+                    throw new Exception("日期格式有誤");
+                    break;
+                case 1:
+                    s = Convert.ToInt32(temp[1]);
+                    if (s>60)
+                    {
+                        
+                        m = s / 60;
+                        s = s % 60;
+                        if (m>60)
+                        {
+                            h = m / 60;
+                            m = m % 60;
+
+                            return $"{(h).ToString()} {(m).ToString()} {(s).ToString()}";
+                        }
+                        else
+                        {
+                            return $"{(m).ToString()} {(s).ToString()}";
+                        }
+                    }
+                    else
+                    {
+                        return $"{(s).ToString()}";
+                    }
+
+
+                    
+                case 2:
+
+                    m = Convert.ToInt32(temp[0]);
+                    s = Convert.ToInt32(temp[1]);
+
+                    if (m>60)
+                    {
+                        h = m / 60;
+                        m = m % 60;
+
+                    }
+
+                    return $"{(m).ToString()} {(s).ToString()}";
+
+
+
+
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void apply_Click(object sender, RoutedEventArgs e)
+        {
+            int selected = json_lv.SelectedIndex;
+            if (selected != -1)
+            {
+
+                timetable[selected].ED_Start = TimeEncoder(edStart_tb.Text);
+                timetable[selected].ED_End= TimeEncoder(edEnd_tb.Text);
+                timetable[selected].OP_Start= TimeEncoder(opStart_tb.Text);
+                timetable[selected].OP_End= TimeEncoder(opEnd_tb.Text);
+                timetable[selected].sn= sn_tb.Text;
+                save();
+            }
+         
+        }
+        private void save()
+        {
+
+        }
     }
 }
